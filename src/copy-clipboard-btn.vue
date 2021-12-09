852473a0e308b18e4e35daa2cbce3203ebddf79c
Sophie Su <Sophie.Su@frankyad.com>
2021-12-09 15:23:03 +0800

    fix: fix click event handle

--- src/copy-clipboard-btn.vue
+++ src/copy-clipboard-btn.vue
@@ -1,11 +1,13 @@
 <template>
-  <button :data-clipboard-text="text" class="btn copy-clipboard" :style="styleOptions" @click="showCopyAlert($event)"
-          type="button">
-    <svg height="15" viewBox="0 0 24 24" width="15" xmlns="http://www.w3.org/2000/svg">
-      <path d="M18 6v-6h-18v18h6v6h18v-18h-6zm-12 10h-4v-14h14v4h-10v10zm16 6h-14v-14h14v14z"/>
-    </svg>
-    <slot></slot>
-  </button>
+  <div class="copy-clipboard-wrapper">
+    <button :data-clipboard-text="text" class="btn copy-clipboard" :style="styleOptions" @click="showCopyAlert($event)"
+            type="button">
+      <svg height="15" viewBox="0 0 24 24" width="15" xmlns="http://www.w3.org/2000/svg">
+        <path d="M18 6v-6h-18v18h6v6h18v-18h-6zm-12 10h-4v-14h14v4h-10v10zm16 6h-14v-14h14v14z"/>
+      </svg>
+      <slot></slot>
+    </button>
+  </div>
 </template>
 
 <script>
