b7a8aa081d550ec83dba6deccb10cbc874f8676a
Sophie Su <Sophie.Su@frankyad.com>
2021-12-09 16:21:33 +0800

    feat: make alertText's position can be customized

--- src/copy-clipboard-btn.vue
+++ src/copy-clipboard-btn.vue
@@ -1,13 +1,11 @@
 <template>
-  <div class="copy-clipboard-wrapper">
-    <button :data-clipboard-text="text" class="btn copy-clipboard" :style="styleOptions" @click="showCopyAlert($event)"
-            type="button">
-      <svg height="15" viewBox="0 0 24 24" width="15" xmlns="http://www.w3.org/2000/svg">
-        <path d="M18 6v-6h-18v18h6v6h18v-18h-6zm-12 10h-4v-14h14v4h-10v10zm16 6h-14v-14h14v14z"/>
-      </svg>
-      <slot></slot>
-    </button>
-  </div>
+  <button :data-clipboard-text="text" class="btn copy-clipboard" :style="styleOptions" @click="showCopyAlert($event)"
+          type="button">
+    <svg height="15" viewBox="0 0 24 24" width="15" xmlns="http://www.w3.org/2000/svg">
+      <path d="M18 6v-6h-18v18h6v6h18v-18h-6zm-12 10h-4v-14h14v4h-10v10zm16 6h-14v-14h14v14z"/>
+    </svg>
+    <slot></slot>
+  </button>
 </template>
 
 <script>
@@ -16,13 +14,14 @@ export default {
     styleOptions: {},
   }),
   props: {
-    text: String,
     copyAlertText: String,
+    copyAlertTarget: String,
     options: Object,
+    text: String,
   },
   methods: {
     showCopyAlert(e) {
-      const div = e.target.closest(".copy-clipboard-wrapper");
+      const div = e.target.closest(this.copyAlertTarget || 'div');
       div.classList.add('show-copy-text');
       div.setAttribute('data-copy-text', this.copyAlertText);
       setTimeout(() => {
@@ -51,7 +50,7 @@ export default {
   fill: black;
 }
 
-.copy-clipboard-wrapper.show-copy-text::after {
+.show-copy-text::after {
   content: attr(data-copy-text);
   margin-left: .5rem;
 }
