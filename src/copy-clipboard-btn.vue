b75f26a6436eb09f39eb466e72c4871d11c13a45
Sophie Su <Sophie.Su@frankyad.com>
2021-12-09 14:54:43 +0800

    fix: fix style

--- src/copy-clipboard-btn.vue
+++ src/copy-clipboard-btn.vue
@@ -1,5 +1,6 @@
 <template>
-  <button :data-clipboard-text="text" class="btn copy-clipboard" :style="styleOptions" @click="showCopyAlert($event)">
+  <button :data-clipboard-text="text" class="btn copy-clipboard" :style="styleOptions" @click="showCopyAlert($event)"
+          type="button">
     <svg height="15" viewBox="0 0 24 24" width="15" xmlns="http://www.w3.org/2000/svg">
       <path d="M18 6v-6h-18v18h6v6h18v-18h-6zm-12 10h-4v-14h14v4h-10v10zm16 6h-14v-14h14v14z"/>
     </svg>
@@ -36,8 +37,8 @@ export default {
 <style>
 .copy-clipboard {
   background: white;
-  border-color: #fff;
-  fill: #fff;
+  border-color: #000;
+  fill: #000;
   line-height: 0;
   padding: .5rem;
 
