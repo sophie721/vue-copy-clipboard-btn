85b32bad51c331491aba91855c65b9007bed2d2d
Sophie Su <Sophie.Su@frankyad.com>
2021-12-09 14:59:10 +0800

    feat: add slot after btn icon

--- src/copy-clipboard-btn.vue
+++ src/copy-clipboard-btn.vue
@@ -4,6 +4,7 @@
     <svg height="15" viewBox="0 0 24 24" width="15" xmlns="http://www.w3.org/2000/svg">
       <path d="M18 6v-6h-18v18h6v6h18v-18h-6zm-12 10h-4v-14h14v4h-10v10zm16 6h-14v-14h14v14z"/>
     </svg>
+    <slot></slot>
   </button>
 </template>
 
