0c70eb8676c96ddd4ee69a80fe73c45597907e7d
Sophie Su <Sophie.Su@frankyad.com>
2021-12-10 18:18:53 +0800

    feat: add ClipboardJS & fix alert to be tooltip

--- src/copy-clipboard-btn.vue
+++ src/copy-clipboard-btn.vue
@@ -1,5 +1,5 @@
 <template>
-  <button :data-clipboard-text="text" class="btn copy-clipboard" :style="styleOptions" @click="showCopyAlert($event)"
+  <button :data-clipboard-text="text" class="copy-clipboard" :style="styleOptions" @click="showCopyAlert($event)"
           type="button">
     <svg height="15" viewBox="0 0 24 24" width="15" xmlns="http://www.w3.org/2000/svg">
       <path d="M18 6v-6h-18v18h6v6h18v-18h-6zm-12 10h-4v-14h14v4h-10v10zm16 6h-14v-14h14v14z"/>
@@ -9,7 +9,17 @@
 </template>
 
 <script>
+import ClipboardJS from 'clipboard'
+
 export default {
+  computed: {
+    cssProps() {
+      return Object.assign({
+        '--tooltip-bg-color': '#24292f',
+        '--tooltip-color': '#ffffff',
+      }, this.options)
+    },
+  },
   data: () => ({
     styleOptions: {},
   }),
@@ -21,23 +31,26 @@ export default {
   },
   methods: {
     showCopyAlert(e) {
-      const div = e.target.closest(this.copyAlertTarget || 'div');
-      div.classList.add('show-copy-text');
+      const div = e.target.closest('.copy-clipboard');
+      div.classList.add('tooltipped');
       div.setAttribute('data-copy-text', this.copyAlertText);
       setTimeout(() => {
-        div.classList.remove('show-copy-text');
+        div.classList.remove('tooltipped');
         div.removeAttribute('data-copy-text');
       }, 2000);
     },
   },
-  mounted() {
-    this.styleOptions = this.options || {}
+  watch: {
+    text() {
+      new ClipboardJS('.copy-clipboard')
+    },
   },
 }
 </script>
 
 <style>
 .copy-clipboard {
+  position: relative;
   line-height: 0;
   padding: .5rem;
   fill: black;
@@ -48,12 +61,34 @@ export default {
   fill-opacity: 1;
 }
 
-.show-copy-text::after {
+.copy-clipboard.tooltipped::after {
   content: attr(data-copy-text);
-  margin-left: .5rem;
-  height: 100%;
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
+  position: absolute;
+  top: 100%;
+  right: 50%;
+  margin-top: 6px;
+  z-index: 1000000;
+  display: none;
+  padding: .5em .75em;
+  font-size: 80%;
+  color: var(--tooltip-color, black);
+  text-align: center;
+  text-shadow: none;
+  text-decoration: none;
+  text-transform: none;
+  word-wrap: break-word;
+  white-space: pre;
+  pointer-events: none;
+  background: var(--tooltip-bg-color, white);
+  border-radius: 6px;
+  opacity: 0
+}
+
+.copy-clipboard.tooltipped:hover::after {
+  display: inline-block;
+  animation-name: tooltip-appear;
+  animation-duration: .1s;
+  animation-fill-mode: forwards;
+  animation-timing-function: ease-in;
 }
 </style>
