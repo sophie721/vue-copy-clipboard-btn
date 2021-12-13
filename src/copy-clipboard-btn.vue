5dda856e8560968dc641c2df82810a8b9a6eb971
Sophie Su <Sophie.Su@frankyad.com>
2021-12-13 12:14:46 +0800

    fix: fix style

--- src/copy-clipboard-btn.vue
+++ src/copy-clipboard-btn.vue
@@ -1,5 +1,5 @@
 <template>
-  <button :data-clipboard-text="text" class="copy-clipboard" :style="styleOptions" @click="showCopyAlert($event)"
+  <button :data-clipboard-text="text" class="copy-clipboard" :style="cssProps" @click="showCopyAlert($event)"
           type="button">
     <svg height="15" viewBox="0 0 24 24" width="15" xmlns="http://www.w3.org/2000/svg">
       <path d="M18 6v-6h-18v18h6v6h18v-18h-6zm-12 10h-4v-14h14v4h-10v10zm16 6h-14v-14h14v14z"/>
@@ -20,9 +20,6 @@ export default {
       }, this.options)
     },
   },
-  data: () => ({
-    styleOptions: {},
-  }),
   props: {
     copyAlertText: String,
     copyAlertTarget: String,
@@ -59,6 +56,27 @@ export default {
 
 .copy-clipboard:not(:disabled):hover {
   fill-opacity: 1;
+}
+
+.copy-clipboard.tooltipped::before {
+  content: "";
+  position: absolute;
+  top: auto;
+  right: 50%;
+  bottom: -7px;
+  margin-right: -6px;
+  transform: translateX(50%);
+  z-index: 1000001;
+  display: none;
+  border: 6px solid var(--tooltip-bg-color);
+}
+
+.copy-clipboard.tooltipped:hover::before {
+  display: inline-block;
+  animation-name: tooltip-appear;
+  animation-duration: .1s;
+  animation-fill-mode: forwards;
+  animation-timing-function: ease-in;
 }
 
 .copy-clipboard.tooltipped::after {
